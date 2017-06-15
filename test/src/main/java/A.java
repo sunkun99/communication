047e5318a3a08f90658c91e69330f29ea7cc85e2
sunkun <sunkun@jk.cn>
2017-06-15 19:16:43 +0800

    测试

--- test/src/main/java/A.java
+++ test/src/main/java/A.java
@@ -5,54 +5,5 @@
  * Time: 19:59
  */
 public class A {
-	private String flowId;
 
-	/*private String flowOwner;
-
-	private String flowResult;
-
-	private String flowEndDate;
-*/
-	public String getFlowId() {
-		return flowId;
-	}
-
-	public void setFlowId(String flowId) {
-		this.flowId = flowId;
-	}
-
-	/*public String getFlowOwner() {
-		return flowOwner;
-	}
-
-	public void setFlowOwner(String flowOwner) {
-		this.flowOwner = flowOwner;
-	}
-
-	public String getFlowResult() {
-		return flowResult;
-	}
-
-	public void setFlowResult(String flowResult) {
-		this.flowResult = flowResult;
-	}
-
-	public String getFlowEndDate() {
-		return flowEndDate;
-	}
-
-	public void setFlowEndDate(String flowEndDate) {
-		this.flowEndDate = flowEndDate;
-	}
-
-	@Override
-	public String toString() {
-		final StringBuilder sb = new StringBuilder("A{");
-		sb.append("flowId='").append(flowId).append('\'');
-		sb.append(", flowOwner='").append(flowOwner).append('\'');
-		sb.append(", flowResult='").append(flowResult).append('\'');
-		sb.append(", flowEndDate='").append(flowEndDate).append('\'');
-		sb.append('}');
-		return sb.toString();
-	}*/
 }
